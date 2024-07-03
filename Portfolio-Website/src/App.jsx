c58165d9c72c481f1ffb3b6c3421a88a445974b4
Inko-z <rodolfoandresgp@hotmail.com>
2024-07-03 00:42:53 -0600

    Introcard element format

--- Portfolio-Website/src/App.jsx
+++ Portfolio-Website/src/App.jsx
@@ -1,11 +1,11 @@
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
-import Card from "./Card.jsx";
+import IntroCard from "./IntroCard.jsx";
 function App() {
   return(
     <>
-      <Header/>
-      <Card/>
+      
+      <IntroCard/>
       <Footer/>
     </>
     );
---
+++ Portfolio-Website/src/IntroCard.jsx
@@ -0,0 +1,19 @@
+import profile_Pic from './assets/profilePic.jpg';
+
+function IntroCard() {
+  return (
+    <div className="IntroCard">
+
+        <div className='IntroCardPfp'>
+            <img src={profile_Pic} alt="Profile picture" />
+        </div>
+        <div className="IntroCardText">
+            <h2>Hi, I{"'"}m</h2>
+            <h1>Rodolfo Gil-Preira</h1>
+            <p>3rd year Software Engineering student</p>
+        </div>  
+    </div>
+  );
+}
+
+export default IntroCard;
