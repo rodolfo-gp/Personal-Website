5b0abb7f9a1e3998fb9e09c51dc6692ddd940398
Inko-z <rodolfoandresgp@hotmail.com>
2024-07-24 23:11:01 -0600

    Added resume btn

--- Portfolio-Website/src/components/Hero/Hero.jsx
+++ Portfolio-Website/src/components/Hero/Hero.jsx
@@ -1,5 +1,4 @@
-//import React from "react";
-
+// import React from "react";
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
@@ -13,17 +12,26 @@ export const Hero = () => {
       />
 
       <div className={styles.content}>
-        <h1 className={styles.title}>Hey, I'm 
-                                <br/>Rodolfo Gil-Pereira</h1>
+        <h1 className={styles.title}>
+          Hey, I'm 
+          <br />
+          Rodolfo Gil-Pereira
+        </h1>
         <p className={styles.description}>
-          I'm a 3rd Year Software Engineering Student<br/>
-          at the University of Calgary
+          I'm a 3rd Year Software Engineering Student
+          <br />
+          at the University of Calgary.
         </p>
-        <a href="#contact" className={styles.contactBtn}>
-          Contact Me
-        </a>
+        <div className={styles.buttons}>
+          <a href="#contact" className={styles.contactBtn}>
+            Contact
+          </a>
+          <a href="/public/Rodolfo_Resume.pdf" className={styles.resumeBtn} target="_blank">
+            Resume
+          </a>
+        </div>
       </div>
-      
+
       <div className={styles.topBlur} />
       <div className={styles.bottomBlur} />
     </section>
